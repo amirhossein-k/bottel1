1e961f78e683c01cbd988eee0ce5d75dada770a7
amirhossein-k <amirhossein.k787878@gmail.com>
2026-06-03 23:49:43 +0330

    rrrr

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -7,7 +7,7 @@ let cached = global.mongoose;
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
-export async function dbConnect() {
+export async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
